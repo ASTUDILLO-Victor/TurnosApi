5da8d18d86aaeafc11f41da1b57e65c07c260915
vastudillo <vastudillo@viamatica.com>
2026-07-17 11:34:34 -0500

    Agregar archivo de solucion para que CI compile todos los proyectos

--- TurnosApi.Tests/obj/Debug/net10.0/TurnosApi.Tests.AssemblyInfo.cs
+++ TurnosApi.Tests/obj/Debug/net10.0/TurnosApi.Tests.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TurnosApi.Tests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+612d8780882d5fbdd01088a6b7711ed73cefebd9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+465826141cc00a2f9205b513eae985a9367ab813")]
 [assembly: System.Reflection.AssemblyProductAttribute("TurnosApi.Tests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TurnosApi.Tests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
